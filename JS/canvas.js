4f5a4f464847bc8c0638b98209834741792a9af3
jclau69 <jclau69@outlook.com>
2020-11-06 16:03:48 +0100

    tuning canvas

--- JS/canvas.js
+++ JS/canvas.js
@@ -7,77 +7,29 @@ function draw() {
     var ctx = canvas.getContext('2d');
 
 
-    ctx.beginPath();
+for(var i = 0; i <= 5; i++) {
+
+    ctx.beginPath()
+    ctx.moveTo(0, i*40);
+    ctx.lineTo(600, i*40);
+
 
     ctx.strokeStyle = 'lightgrey';
-    ctx.lineWidth = 1;
-
-    ctx.moveTo(0, 0);
-    ctx.lineTo(600, 0);
-
-    ctx.moveTo(0, 40);
-    ctx.lineTo(600, 40);
-
-    ctx.moveTo(0, 80);
-    ctx.lineTo(600, 80);
-
-    ctx.moveTo(0, 120);
-    ctx.lineTo(600, 120);
-
-    ctx.moveTo(0, 160);
-    ctx.lineTo(600, 160);
-
-    ctx.moveTo(0, 200);
-    ctx.lineTo(600, 200);
-
     ctx.stroke();
 
-
-    ctx.beginPath();
-
-    ctx.strokeStyle = 'lightgrey';
-    ctx.lineWidth = 1;
-
-    ctx.moveTo(0, 0);
-    ctx.lineTo(0, 200);
-
-    ctx.moveTo(60, 0);
-    ctx.lineTo(60, 200);
-
-    ctx.moveTo(120, 0);
-    ctx.lineTo(120, 200);
-
-    ctx.moveTo(180, 0);
-    ctx.lineTo(180, 200);
-
-    ctx.moveTo(240, 0);
-    ctx.lineTo(240, 200);
-
-    ctx.moveTo(300, 0);
-    ctx.lineTo(300, 200);
-
-    ctx.moveTo(360, 0);
-    ctx.lineTo(360, 200);
-
-    ctx.moveTo(420, 0);
-    ctx.lineTo(420, 200);
-
-    ctx.moveTo(480, 0);
-    ctx.lineTo(480, 200);
-
-    ctx.moveTo(540, 0);
-    ctx.lineTo(540, 200);
-
-    ctx.moveTo(600, 0);
-    ctx.lineTo(600, 200);
-
-
-
-
-    ctx.stroke();
-
-
-
-
-        }
 }
+
+for(var i = 0; i <= 11; i++) {
+
+    ctx.beginPath()
+    ctx.moveTo(i*60, 0);
+    ctx.lineTo(i*60, 200);
+
+
+    ctx.strokeStyle = 'lightgrey';
+    ctx.stroke();
+
+}
+
+}
+  }
